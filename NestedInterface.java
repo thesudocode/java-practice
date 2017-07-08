32376e1df61ff0e629cb6e191ae336eb36cba65e
Nikhil Arora <narora200@gmail.com>
2017-07-08 23:35:17 +0530

    Java Nested Interface Example

---
+++ NestedInterface.java
@@ -0,0 +1,24 @@
+package com.iamoperand.javaConcepts;
+
+/**
+ * Created by iamoperand on 8/7/17.
+ */
+
+interface Interface{
+    void dontshow();
+    interface AnotherInterface{
+        void show();
+    }
+}
+public class NestedInterface implements Interface.AnotherInterface {
+
+
+    public void show() {
+        System.out.println("Shown");
+    }
+
+    public static void main(String[] args){
+        Interface.AnotherInterface ni = new NestedInterface();
+        ni.show();
+    }
+}
