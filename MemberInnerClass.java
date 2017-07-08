b0207ee03051a7f517c70ce709d658b2e189be90
Nikhil Arora <narora200@gmail.com>
2017-07-08 23:00:53 +0530

    Java Member Inner Class Example (Comments Added)

--- MemberInnerClass.java
+++ MemberInnerClass.java
@@ -14,6 +14,14 @@ public class MemberInnerClass {
 
     public static void main(String args[]){
         MemberInnerClass mic = new MemberInnerClass();
+
+        /*
+        Left side always have class name
+        */
+        /*
+        Right side always have objectInstance.method() in case of method
+        But in this case, it has to have a new instance of the class. Thus objectInstance. new ClassName
+        */
         MemberInnerClass.RealMemberInnerClass rmic = mic.new RealMemberInnerClass();
         rmic.printMsg();
     }
