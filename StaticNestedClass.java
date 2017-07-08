c3ea0e8f26bd87117cf7583c39e74d69fdc0f4a1
Nikhil Arora <narora200@gmail.com>
2017-07-08 23:28:30 +0530

    Java Static Nested Class Example (Comments Added)

--- StaticNestedClass.java
+++ StaticNestedClass.java
@@ -3,6 +3,11 @@ package com.iamoperand.javaConcepts;
 /**
  * Created by iamoperand on 8/7/17.
  */
+/*
+A static class i.e. created inside a class is called static nested class in java. It cannot access non-static data members and methods. It can be accessed by outer class name.
+- It can access static data members of outer class including private.
+- Static nested class cannot access non-static (instance) data member or method.
+*/
 public class StaticNestedClass {
 
     static private int data = 3;
